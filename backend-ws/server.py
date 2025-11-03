da579f45edd1b3c853861a7c83248698039a36b6
Parsa <parsam280xyz@gmail.com>
2025-11-03 14:48:20 +0330

    feat:added ws server for routing messages

--- backend-ws/server.py
+++ backend-ws/server.py
@@ -1,1 +1,27 @@
-import asyncio 
+import asyncio
+import websockets
+import json
+
+
+
+# when a new user connects to the server after a successful handshake it adds their socket object to the active_connections 
+active_connections = {} 
+
+
+async def handler(ws):
+    async for message in ws:
+        input_data = json.loads(message)
+        username = input_data['username']
+        to = active_connections.get(input_data['to'])
+        if to:
+            await to.send(input_data['message'])
+        active_connections[username] = ws
+
+async def main():
+    async with websockets.serve(handler,"localhost",8090):
+        print("[+] Server is running at localhost:8070")
+        await asyncio.Future()
+
+asyncio.run(main())
+
+
