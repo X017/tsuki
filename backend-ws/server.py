7d82d4e52cdc6051292dcd01c78d8dec2b1f2806
Parsa <parsam280xyz@gmail.com>
2025-11-04 15:38:36 +0330

    fix:altered server structure for a new protocol handshake

--- backend-ws/server.py
+++ backend-ws/server.py
@@ -1,27 +1,25 @@
 import asyncio
 import websockets
-import json
+import cbor 
 
-
-
-# when a new user connects to the server after a successful handshake it adds their socket object to the active_connections 
-active_connections = {} 
+# when a new user connects to the server after a successful handshake it adds their socket object to the active_connections
+active_connections = {}
 
 
 async def handler(ws):
     async for message in ws:
-        input_data = json.loads(message)
-        username = input_data['username']
-        to = active_connections.get(input_data['to'])
-        if to:
-            await to.send(input_data['message'])
-        active_connections[username] = ws
+        incoming_data = cbor.loads(message)
+        print(incoming_data)
+        active_connections[ws] = incoming_data
+        
+
+
 
 async def main():
-    async with websockets.serve(handler,"localhost",8090):
+    async with websockets.serve(handler, "localhost", 8090):
         print("[+] Server is running at localhost:8070")
         await asyncio.Future()
 
-asyncio.run(main())
 
+asyncio.run(main())
 
