190e263be991a6744d64b9d226026a552b651bfb
Parsa <parsam280xyz@gmail.com>
2025-11-12 15:11:56 +0330

    fix:server router is now actually routes messages

--- backend-ws/server.py
+++ backend-ws/server.py
@@ -2,22 +2,27 @@ import asyncio
 import websockets
 import cbor 
 
-# when a new user connects to the server after a successful handshake it adds their socket object to the active_connections
 active_connections = {}
 
 
 async def handler(ws):
     async for message in ws:
         incoming_data = cbor.loads(message)
-        print(incoming_data)
-        active_connections[ws] = incoming_data
-        
-
-
+        if incoming_data['request'] == 'handshake':
+            user_info = incoming_data.get('data')
+            active_connections[user_info['username']] = {'conneciton':ws,'public_key':user_info['public_key'],'stamp':user_info['stamp']}
+            print(active_connections)
+        if incoming_data['request'] == 'message':
+            if incoming_data['data']['to']:
+                sent_to = active_connections[incoming_data['data']['to']]
+                ws = sent_to['conneciton']
+                sent_to_data = incoming_data['data']
+                serialized_sent_to = cbor.dumps(sent_to_data)
+                await ws.send(serialized_sent_to)
 
 async def main():
     async with websockets.serve(handler, "localhost", 8090):
-        print("[+] Server is running at localhost:8070")
+        print("[+] Server is running at localhost:8090")
         await asyncio.Future()
 
 
